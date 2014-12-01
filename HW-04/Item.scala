a87cbe29468eb46c0af355e45e1e8a1c88c5e048
Yusuf Biber <yusuf_biber@hotmail.com>
2014-12-01 21:36:32 +0200

    Create Item.scala

---
+++ HW-04/Item.scala
@@ -0,0 +1,11 @@
+/*
+ Yusuf Biber
+ 160201018
+ CENG432
+ Homework 4 - Item
+  */
+
+class Item(val keyOfItem:String,val valueOfItem:String) {	//Item object that will be added to the IyteHashTable
+  val key=keyOfItem
+  val value=valueOfItem 
+}
